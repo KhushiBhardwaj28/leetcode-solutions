812dcc655e424db576051a6594e84df7230322dd
KhushiBhardwaj28 <163660656+KhushiBhardwaj28@users.noreply.github.com>
2025-08-27 07:50:35 +0530

    File updated at 27/8/2025, 7:50:32 am

--- 222-CountCompleteTreeNodes/222-CountCompleteTreeNodes.java
+++ 222-CountCompleteTreeNodes/222-CountCompleteTreeNodes.java
@@ -1,4 +1,4 @@
-// Last updated: 27/8/2025, 7:46:12 am
+// Last updated: 27/8/2025, 7:50:32 am
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -15,38 +15,66 @@
  * }
  */
 class Solution {
-    public int countNodes(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-        int lh = findHeightLeft(root);
-        int rh = findHeightRight(root);
-        if (lh == rh) {
-            return (1 << lh) - 1; 
-        }
-     
-        return 1 + countNodes(root.left) + countNodes(root.right);
+    public int amountOfTime(TreeNode root, int start) {
+        HashMap<TreeNode, TreeNode> mpp = new HashMap<>();
+        TreeNode target = bfsToMapParents(root, mpp, start);
+        int maxi = findMaxDistance(mpp, target);
+        return maxi;
     }
-    
-    // Function to find the left height of a tree
-    private int findHeightLeft(TreeNode node) {
-        int height = 0;
-        // Traverse left child until
-        // reaching the leftmost leaf
-        while (node != null) {
-            height++;
-            node = node.left;
+    private TreeNode bfsToMapParents(TreeNode root, HashMap<TreeNode, TreeNode> mpp, int start) {
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        TreeNode res = new TreeNode(-1);
+
+        while (!q.isEmpty()) {
+            TreeNode node = q.poll();
+            if (node.val == start) res = node;
+            if (node.left != null) {
+                mpp.put(node.left, node);
+                q.offer(node.left);
+            }
+            if (node.right != null) {
+                mpp.put(node.right, node);
+                q.offer(node.right);
+            }
         }
-        return height;
+        return res;
     }
-    private int findHeightRight(TreeNode node) {
-        int height = 0;
-        // Traverse right child until
-        // reaching the rightmost leaf
-        while (node != null) {
-            height++;
-            node = node.right;
+
+    private int findMaxDistance(HashMap<TreeNode, TreeNode> mpp, TreeNode target) {
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(target);
+        HashMap<TreeNode, Integer> vis = new HashMap<>();
+        vis.put(target, 1);
+        int maxi = 0;
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            int fl = 0;
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+
+                if (node.left != null && vis.get(node.left) == null) {
+                    fl = 1;
+                    vis.put(node.left, 1);
+                    q.offer(node.left);
+                }
+
+                if (node.right != null && vis.get(node.right) == null) {
+                    fl = 1;
+                    vis.put(node.right, 1);
+                    q.offer(node.right);
+                }
+                if (mpp.get(node) != null && vis.get(mpp.get(node)) == null) {
+                    fl = 1;
+                    vis.put(mpp.get(node), 1);
+                    q.offer(mpp.get(node));
+                }
+            }
+            // Increment max distance if any node was burned
+            if (fl == 1) maxi++;
         }
-        return height;
+        return maxi;
     }
 }

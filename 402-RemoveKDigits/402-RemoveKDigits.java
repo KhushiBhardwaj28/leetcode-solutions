5fa9d20ed9e71fcf556700eacad0ac6673a5f5c9
KhushiBhardwaj28 <163660656+KhushiBhardwaj28@users.noreply.github.com>
2025-09-02 19:57:28 +0530

    File updated at 2/9/2025, 7:57:25 pm

--- 402-RemoveKDigits/402-RemoveKDigits.java
+++ 402-RemoveKDigits/402-RemoveKDigits.java
@@ -1,34 +1,20 @@
-// Last updated: 2/9/2025, 7:54:49 pm
+// Last updated: 2/9/2025, 7:57:25 pm
 class Solution {
-    public String removeKdigits(String num, int k) {
-        Stack<Character> st = new Stack<>(); 
-        for(int i = 0; i < num.length(); i++) {    
-            char digit = num.charAt(i);
-            while(!st.isEmpty() && k > 0&& st.peek() > digit) {
-                st.pop();
-                k--; 
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+        Arrays.fill(ans, -1);
+       
+        for(int i = 0; i < n; i++) {
+            int currEle = nums[i];
+            for(int j = 1; j < n; j++) {
+                int ind = (j + i) % n;
+                if(nums[ind] > currEle) {
+                    ans[i] = nums[ind];
+                    break;
+                }    
             }
-            st.push(digit);
         }
-        while(!st.isEmpty() && k > 0) {
-            
-            st.pop();
-            k--; 
-        }
-        
-        if(st.isEmpty()) return "0";
-        StringBuilder res = new StringBuilder();
-        while(!st.isEmpty()) {
-            res.append(st.pop());
-        }
-        
-        while(res.length() > 0 && 
-              res.charAt(res.length() - 1) == '0') {
-
-            res.deleteCharAt(res.length() - 1);
-        }
-        res.reverse();
-        if(res.length() == 0) return "0";
-        return res.toString();
+        return ans;
     }
 }

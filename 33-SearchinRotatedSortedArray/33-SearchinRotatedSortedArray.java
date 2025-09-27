11f6b45c81f06325b7e4cd9282ec2d6f5c819caa
KhushiBhardwaj28 <163660656+KhushiBhardwaj28@users.noreply.github.com>
2025-09-27 14:40:17 +0530

    File updated at 27/9/2025, 2:40:17 pm

--- 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.java
+++ 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.java
@@ -1,25 +1,20 @@
-// Last updated: 27/9/2025, 2:23:55 pm
+// Last updated: 27/9/2025, 2:40:17 pm
 class Solution {
     public String frequencySort(String s) {
-        HashMap<Character, Integer> map = new HashMap<>();
-        
-        for(int i = 0; i<s.length(); i++){
-            char ch = s.charAt(i);
-            map.put(ch, map.getOrDefault(ch, 0)+1);
+        HashMap<Character,Integer> map=new HashMap<>();
+        for(char ch:s.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0)+1);
         }
-
-        ArrayList<Map.Entry<Character, Integer>> ll = new ArrayList<>(map.entrySet());
-        ll.sort((a,b)->b.getValue() - a.getValue());
-        StringBuilder sb = new StringBuilder();
-        for(int i =0; i<ll.size(); i++){
-            char ch = ll.get(i).getKey();
-            int freq = ll.get(i).getValue();
-            while(freq-->0){
+        PriorityQueue<Character> pq=new PriorityQueue<>((a,b)->map.get(b)-map.get(a));
+        pq.addAll(map.keySet());
+        StringBuilder sb=new StringBuilder();
+        while(!pq.isEmpty()){
+            char ch=pq.poll();
+            int fre=map.get(ch);
+            while(fre-->0){
                 sb.append(ch);
             }
         }
         return sb.toString();
     }
-
-   
 }

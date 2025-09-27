3d93397795079313fc3c7139f1a04bfd7d938505
KhushiBhardwaj28 <163660656+KhushiBhardwaj28@users.noreply.github.com>
2025-09-27 22:06:42 +0530

    File updated at 27/9/2025, 10:06:39 pm

--- 452-MinimumNumberofArrowstoBurstBalloons/452-MinimumNumberofArrowstoBurstBalloons.java
+++ 452-MinimumNumberofArrowstoBurstBalloons/452-MinimumNumberofArrowstoBurstBalloons.java
@@ -1,16 +1,48 @@
-// Last updated: 26/9/2025, 10:00:09 pm
+// Last updated: 27/9/2025, 10:06:39 pm
 class Solution {
-    public int findMinArrowShots(int[][] points) {
-        Arrays.sort(points, (a,b)->Integer.compare(a[1],b[1]));
-        if(points.length == 0) return 0;
-        int arr = 1;
-        int arrPos = points[0][1];
-        for(int i = 1; i<points.length; i++){
-            if(points[i][0]>arrPos){
-                arr++;
-                arrPos = points[i][1];
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int l = nums.length;
+        int[] ans = new int[l-k+1];
+        if(k==1){
+            return nums;
+        }
+        int max = Integer.MIN_VALUE;
+        int idx = 0;
+        for(int i = 0; i<k; i++){
+            int n = nums[i];
+            if(n>=max){
+                idx = i;
+                max = n;
             }
         }
-        return arr;
+        ans[0] = max;
+        int end = 0;
+        for(int i = 1; i<ans.length; i++){
+            end = i+k-1;
+            if(i<=idx){
+                if(nums[end]>=max){
+                    max = nums[end];
+                    idx = end;
+                }
+            }else{
+                if(nums[end]>=max-1){
+                    max = nums[end];
+                    idx = end;
+                }else if(nums[i]>=max-1){
+                    max = nums[i];
+                    idx = i;
+                }else{
+                    max = Integer.MIN_VALUE;
+                    for(int j = i; j<=end; j++){
+                        if(nums[j]>=max){
+                            max = nums[j];
+                            idx = j;
+                        }
+                    }
+                }
+            }
+            ans[i] = max;
+        }
+        return ans;
     }
 }

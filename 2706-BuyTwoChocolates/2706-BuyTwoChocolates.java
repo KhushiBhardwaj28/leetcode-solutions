d08386e5c8f6fc8d060d9e224bf18e08d2203a38
KhushiBhardwaj28 <163660656+KhushiBhardwaj28@users.noreply.github.com>
2025-10-01 10:47:51 +0530

    File updated at 1/10/2025, 10:47:50 am

--- 2706-BuyTwoChocolates/2706-BuyTwoChocolates.java
+++ 2706-BuyTwoChocolates/2706-BuyTwoChocolates.java
@@ -1,13 +1,22 @@
-// Last updated: 1/10/2025, 10:39:45 am
+// Last updated: 1/10/2025, 10:47:50 am
 class Solution {
     public int buyChoco(int[] prices, int money) {
-        Arrays.sort(prices);
-        int sum = prices[0]+prices[1];
-        if(money-sum>=0){
-            return money-sum;
+        // Arrays.sort(prices);
+        // int sum = prices[0]+prices[1];
+        int min = Integer.MAX_VALUE;
+        int secMin = Integer.MAX_VALUE;
+        for(int x: prices){
+            if(x<min){
+                secMin = min;
+                min = x;
+            }else if(secMin>x){
+                secMin = x;
+            }
+
         }
-        else{
+        if(min+secMin>money){
             return money;
         }
+        return money-(min+secMin);
     }
 }

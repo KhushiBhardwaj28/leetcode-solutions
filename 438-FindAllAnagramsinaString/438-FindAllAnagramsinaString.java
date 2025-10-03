116337190bd271788f273139e4f11b76ab6a58c7
KhushiBhardwaj28 <163660656+KhushiBhardwaj28@users.noreply.github.com>
2025-10-03 11:26:21 +0530

    File updated at 3/10/2025, 11:26:19 am

--- 438-FindAllAnagramsinaString/438-FindAllAnagramsinaString.java
+++ 438-FindAllAnagramsinaString/438-FindAllAnagramsinaString.java
@@ -1,18 +1,18 @@
-// Last updated: 3/10/2025, 10:37:46 am
+// Last updated: 3/10/2025, 11:26:19 am
 class Solution {
-    public int eraseOverlapIntervals(int[][] intervals) {
-
-        Arrays.sort(intervals, (a,b)->Integer.compare(a[1],b[1]));
-        if(intervals.length == 0) return 0;
-        int arr = 1;
-        int arrPos = intervals[0][1];
-        for(int i = 1; i<intervals.length; i++){
-            if(intervals[i][0]>=arrPos){
-                arr++;
-                arrPos = intervals[i][1];
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int i = 0;
+        int j = people.length-1;
+        int minBoat = 0;
+        while(i<=j){
+            if(people[i]+people[j]<=limit){
+                i++;
+                
             }
+            j--;
+            minBoat++;
         }
-        return intervals.length-arr;
+        return minBoat;
     }
 }
-    

6cece3239081e08e06395410fd86fe30657e7a92
KhushiBhardwaj28 <163660656+KhushiBhardwaj28@users.noreply.github.com>
2025-10-06 11:48:23 +0530

    File updated at 6/10/2025, 11:48:21 am

--- 134-GasStation/134-GasStation.java
+++ 134-GasStation/134-GasStation.java
@@ -1,20 +1,21 @@
-// Last updated: 6/10/2025, 11:30:42 am
+// Last updated: 6/10/2025, 11:48:21 am
 class Solution {
-    public int fib(int n) {
-        // int a =0, b=1;
-        // if(n==0 || n==1){
-        //     return n;
-        // }
-        // return fib(n-1)+fib(n-2);
-        if(n==0 || n==1){
-            return n;
+    public int minCostClimbingStairs(int[] cost) {
+        int[] dp = new int[cost.length];
+        Arrays.fill(dp,-1);
+        int zero = climb(dp, 0, cost);
+        int first = climb(dp, 1, cost);
+        return Math.min(zero, first);
+    }
+    public static int climb(int[] dp, int i, int[] arr){
+        if(i>=arr.length){
+            return 0;
         }
-        int[] dp = new int[n+1];
-        dp[0] = 0;
-        dp[1] = 1;
-        for(int i = 2; i<=n; i++){
-            dp[i] = dp[i-1]+dp[i-2];
+        if(dp[i] != -1){
+            return dp[i];
         }
-        return dp[n];
+        int fStair = climb(dp, i+1, arr);
+        int sStair = climb(dp, i+2, arr);
+        return dp[i] = Math.min(fStair,sStair)+arr[i];
     }
 }

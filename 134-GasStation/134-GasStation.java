a9c85d87ac2c7f7b2164c2491b95a689fb012028
KhushiBhardwaj28 <163660656+KhushiBhardwaj28@users.noreply.github.com>
2025-10-06 11:30:44 +0530

    File updated at 6/10/2025, 11:30:42 am

--- 134-GasStation/134-GasStation.java
+++ 134-GasStation/134-GasStation.java
@@ -1,10 +1,20 @@
-// Last updated: 6/10/2025, 10:30:13 am
+// Last updated: 6/10/2025, 11:30:42 am
 class Solution {
     public int fib(int n) {
-        int a =0, b=1;
+        // int a =0, b=1;
+        // if(n==0 || n==1){
+        //     return n;
+        // }
+        // return fib(n-1)+fib(n-2);
         if(n==0 || n==1){
             return n;
         }
-        return fib(n-1)+fib(n-2);
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i = 2; i<=n; i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 }

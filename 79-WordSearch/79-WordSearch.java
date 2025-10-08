69fefe83eb8ba60c82836e8331ed4ba013540ad0
KhushiBhardwaj28 <163660656+KhushiBhardwaj28@users.noreply.github.com>
2025-10-08 11:44:13 +0530

    File updated at 8/10/2025, 11:44:11 am

--- 79-WordSearch/79-WordSearch.java
+++ 79-WordSearch/79-WordSearch.java
@@ -1,54 +1,20 @@
-// Last updated: 8/10/2025, 11:36:38 am
+// Last updated: 8/10/2025, 11:44:11 am
 class Solution {
-    public boolean exist(char[][] board, String word) {
-        boolean visited[][] = new boolean[board.length][board[0].length];
-        for(int i = 0; i<board.length; i++){
-            for(int j = 0; j<board[0].length; j++){
-                visited[i][j] = true;
-                if(board[i][j]==word.charAt(0) && solution(0,i,j,board,word,visited)){
-                    return true;
-                }
-                visited[i][j] = false;
+    public int characterReplacement(String s, int k) {
+        HashMap<Character,Integer> fre=new HashMap<>();
+        int maxfre=0,j=0,res=0;
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            fre.put(ch,fre.getOrDefault(ch,0)+1);
+            maxfre=Math.max(maxfre,fre.get(ch));
+
+            while((i-j+1)-maxfre>k){
+                char c=s.charAt(j);
+                fre.put(c,fre.get(c)-1);
+                j++;
             }
+            res=Math.max(res,i-j+1);
         }
-        return false;
-    }
-    public boolean solution(int curr, int i, int j, char[][] board, String word, boolean[][] visited){
-        if(curr == word.length()-1){
-            return true;
-        }
-        if(i-1>=0 && board[i-1][j]==word.charAt(curr+1) && !visited[i-1][j]){
-            visited[i-1][j] = true;
-            boolean ans = solution(curr+1, i-1, j, board, word, visited);
-            if(ans){
-                return true;
-            }
-            visited[i-1][j] = false;
-        }
-        if(j-1>=0 && board[i][j-1]==word.charAt(curr+1) && !visited[i][j-1]){
-            visited[i][j-1] = true;
-            boolean ans = solution(curr+1, i, j-1, board, word, visited);
-            if(ans){
-                return true;
-            }
-            visited[i][j-1] = false;
-        }
-        if(i+1<board.length && board[i+1][j]==word.charAt(curr+1) && !visited[i+1][j]){
-            visited[i+1][j] = true;
-            boolean ans = solution(curr+1, i+1, j, board, word, visited);
-            if(ans){
-                return true;
-            }
-            visited[i+1][j] = false;
-        }
-        if(j+1<board[0].length && board[i][j+1]==word.charAt(curr+1) && !visited[i][j+1]){
-            visited[i][j+1] = true;
-            boolean ans = solution(curr+1, i, j+1, board, word, visited);
-            if(ans){
-                return true;
-            }
-            visited[i][j+1] = false;
-        }
-        return false;
+        return res;
     }
 }

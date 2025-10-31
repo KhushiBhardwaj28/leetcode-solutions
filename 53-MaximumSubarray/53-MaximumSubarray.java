fff7a2807a01f64d5416e752520a1c726fbba996
KhushiBhardwaj28 <163660656+KhushiBhardwaj28@users.noreply.github.com>
2025-10-31 13:43:47 +0530

    File updated at 31/10/2025, 1:43:48 pm

--- 53-MaximumSubarray/53-MaximumSubarray.java
+++ 53-MaximumSubarray/53-MaximumSubarray.java
@@ -1,27 +1,36 @@
-// Last updated: 31/10/2025, 1:36:12 pm
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+// Last updated: 31/10/2025, 1:43:48 pm
+import java.util.*;
+
 class Solution {
-    public ListNode deleteDuplicates(ListNode head) {
-        if (head == null)return head;
-        ListNode temp = head;
-        while (temp != null) {
-            if (temp.next != null && temp.val == temp.next.val) {
-                temp.next = temp.next.next;
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> result = new ArrayList<>();
+        
+        int n = p.length();
+        int m = s.length();
+        
+        if (m < n) return result; // edge case
+        
+        int[] pFreq = new int[26];
+        int[] sFreq = new int[26];
+        for (int i = 0; i < n; i++) {
+            pFreq[p.charAt(i) - 'a']++;
+        }
+        
+        int i = 0;
+        for (int j = 0; j < m; j++) {  
+            sFreq[s.charAt(j) - 'a']++;
+            
+            // shrink window if size > n
+            if (j - i + 1 > n) {
+                sFreq[s.charAt(i) - 'a']--;
+                i++;
             }
-            else{
-                temp = temp.next;
+            // freq matches → record index
+            if (j - i + 1 == n && Arrays.equals(pFreq, sFreq)) {
+                result.add(i);
             }
         }
-
-        return head;
+        
+        return result;
     }
 }

41b9c6ac62939f4d35828bf5f21c708af16f22e8
KhushiBhardwaj28 <163660656+KhushiBhardwaj28@users.noreply.github.com>
2025-12-03 21:48:01 +0530

    File updated at 3/12/2025, 9:48:02 pm

--- 213-HouseRobberII/213-HouseRobberII.java
+++ 213-HouseRobberII/213-HouseRobberII.java
@@ -1,20 +1,22 @@
-// Last updated: 3/12/2025, 6:41:04 pm
+// Last updated: 3/12/2025, 9:48:02 pm
 1class Solution {
-2    public int lengthOfLIS(int[] nums) {
-3        int[] lis = new int[nums.length];
-4        Arrays.fill(lis, 1);
-5        for(int i = 1; i<lis.length; i++){
-6            for(int j = i-1; j>=0; j--){
-7                if(nums[j]<nums[i]){
-8                    int l = lis[j];
-9                    lis[i] = Math.max(lis[i],l+1);
-10                }
-11            }
-12        }
-13        int maxi = lis[0];
-14        for(int i = 1; i<nums.length; i++){
-15            maxi = Math.max(lis[i],maxi);
-16        }
-17        return maxi;
-18    }
-19}
+2    public int maxUncrossedLines(int[] nums1, int[] nums2) {
+3        int n = nums1.length;
+4        int m = nums2.length;
+5        int[][] dp = new int[n][m];
+6        for(int[] row: dp){
+7            Arrays.fill(row,-1);
+8
+9        }
+10        return f(n-1,m-1, nums1, nums2, dp);
+11    }
+12    public static int f(int idx1, int idx2, int[] s1, int[] s2, int[][] dp){
+13        if(idx1<0 || idx2<0) return 0;
+14        if(dp[idx1][idx2]!=-1) return dp[idx1][idx2];
+15        if(s1[idx1] == s2[idx2]){
+16            return dp[idx1][idx2] = 1+ f(idx1-1, idx2-1, s1, s2, dp);
+17        }
+18        dp[idx1][idx2] = 0+ Math.max(f(idx1-1, idx2, s1, s2, dp), f(idx1, idx2-1, s1, s2, dp));
+19        return dp[idx1][idx2];
+20    }
+21}

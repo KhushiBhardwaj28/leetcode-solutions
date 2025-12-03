1ea225c50335aad44601bf4f7ff755fb233c18cd
KhushiBhardwaj28 <163660656+KhushiBhardwaj28@users.noreply.github.com>
2025-12-03 18:41:03 +0530

    File updated at 3/12/2025, 6:41:04 pm

--- 213-HouseRobberII/213-HouseRobberII.java
+++ 213-HouseRobberII/213-HouseRobberII.java
@@ -1,14 +1,20 @@
-// Last updated: 3/12/2025, 6:06:25 pm
+// Last updated: 3/12/2025, 6:41:04 pm
 1class Solution {
-2    public int minCostClimbingStairs(int[] cost) {
-3        int[] dp = new int[cost.length];
-4        dp[0] = cost[0];
-5        dp[1] = cost[1];
-6        for(int i = 2; i<cost.length; i++){
-7            int first = cost[i]+dp[i-1];
-8            int second = cost[i]+dp[i-2];
-9            dp[i] = Math.min(first, second);
-10        }
-11        return Math.min(dp[cost.length-2],dp[cost.length-1]);
-12    }
-13}
+2    public int lengthOfLIS(int[] nums) {
+3        int[] lis = new int[nums.length];
+4        Arrays.fill(lis, 1);
+5        for(int i = 1; i<lis.length; i++){
+6            for(int j = i-1; j>=0; j--){
+7                if(nums[j]<nums[i]){
+8                    int l = lis[j];
+9                    lis[i] = Math.max(lis[i],l+1);
+10                }
+11            }
+12        }
+13        int maxi = lis[0];
+14        for(int i = 1; i<nums.length; i++){
+15            maxi = Math.max(lis[i],maxi);
+16        }
+17        return maxi;
+18    }
+19}

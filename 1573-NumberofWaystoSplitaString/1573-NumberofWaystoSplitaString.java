8ef5c4d03b0ffb76623635086567de0b7c85ed7c
KhushiBhardwaj28 <163660656+KhushiBhardwaj28@users.noreply.github.com>
2025-12-23 15:00:24 +0530

    File updated at 23/12/2025, 3:00:23 pm

--- 1573-NumberofWaystoSplitaString/1573-NumberofWaystoSplitaString.java
+++ 1573-NumberofWaystoSplitaString/1573-NumberofWaystoSplitaString.java
@@ -1,30 +1,16 @@
-// Last updated: 23/12/2025, 2:53:47 pm
+// Last updated: 23/12/2025, 3:00:23 pm
 1class Solution {
-2    public String generateTag(String caption) {
-3        String[] words = caption.trim().split("\\s+");
-4        StringBuilder tag = new StringBuilder("#");
-5        for (int i = 0; i < words.length; i++) {
-6            String word = words[i].toLowerCase();
-7            if (word.length() == 0) continue;
-8            if (i == 0) {
-9                tag.append(word);
-10            } else {
-11                tag.append(Character.toUpperCase(word.charAt(0)));
-12                tag.append(word.substring(1));
-13            }
-14        }
-15        StringBuilder cleanTag = new StringBuilder();
-16        for (int i = 0; i < tag.length(); i++) {
-17            char ch = tag.charAt(i);
-18            if (ch == '#' || (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
-19                cleanTag.append(ch);
-20            }
-21        }
-22        if (cleanTag.length() > 100) {
-23            return cleanTag.substring(0, 100);
-24        }
-25
-26        return cleanTag.toString();
-27    }
-28}
-29
+2    public String arrangeWords(String text) {
+3        String[] words = text.toLowerCase().split(" ");
+4        List<String> list = Arrays.asList(words);
+5        Collections.sort(list,(a,b)-> a.length() - b.length());
+6        StringBuilder ans = new StringBuilder();
+7        String one = list.get(0);
+8        ans.append(Character.toUpperCase(one.charAt(0))).append(one.substring(1));
+9
+10        for(int i = 1; i<list.size(); i++){
+11            ans.append(" ").append(list.get(i));
+12        }
+13        return ans.toString();
+14    }
+15}

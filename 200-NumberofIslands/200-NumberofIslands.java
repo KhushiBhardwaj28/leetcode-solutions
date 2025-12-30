6e548cdc87df5b2fbc43b22d4eadb32edd9256aa
KhushiBhardwaj28 <163660656+KhushiBhardwaj28@users.noreply.github.com>
2025-12-30 11:44:06 +0530

    File updated at 30/12/2025, 11:44:05 am

--- 200-NumberofIslands/200-NumberofIslands.java
+++ 200-NumberofIslands/200-NumberofIslands.java
@@ -1,35 +1,29 @@
-// Last updated: 29/9/2025, 11:37:01 pm
-class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
-        List<List<Integer>> ans = new ArrayList<>();
-        for(int i = 0; i<nums.length; i++){
-            if(i>0 && nums[i] == nums[i-1]){
-                continue;
-            }
-            int j = i+1;
-            int k = nums.length-1;
-            while(j<k){
-                int sum = nums[i]+nums[j]+nums[k];
-                if(sum>0){
-                    k--;
-                }else if(sum<0){
-                    j++;
-                }else{
-                    List<Integer> l = new ArrayList<>();
-                    l.add(nums[i]);
-                    l.add(nums[j]);
-                    l.add(nums[k]);
-                    ans.add(l);
-                    j++;
-                    k--;
-                    while (j < k && nums[j] == nums[j - 1]) j++;
-                    while (j < k && nums[k] == nums[k + 1]) k--;
-                }
-            }
-        
-           
-        }
-        return ans;
-    }
-}
+// Last updated: 30/12/2025, 11:44:05 am
+1class Solution {
+2    public int numIslands(char[][] grid) {
+3        int m = grid.length;
+4        int n = grid[0].length;
+5        int c = 0;
+6        for(int i = 0; i<m; i++){
+7            for(int j = 0; j<n; j++){
+8                if(grid[i][j] == '1'){
+9                    c++;
+10                    dfs(i,j,grid);
+11                }
+12            }
+13        }
+14        return c;
+15    }
+16    public static void dfs(int i, int j, char[][] grid){
+17        int r = grid.length;
+18        int c = grid[0].length;
+19        if(i<0 || j<0 || i>r-1 || j>c-1|| grid[i][j] == '0'){
+20            return;
+21        }
+22        grid[i][j] = '0';
+23        dfs(i+1, j, grid);
+24        dfs(i-1, j, grid);
+25        dfs(i, j-1, grid);
+26        dfs(i, j+1, grid);
+27    }
+28}

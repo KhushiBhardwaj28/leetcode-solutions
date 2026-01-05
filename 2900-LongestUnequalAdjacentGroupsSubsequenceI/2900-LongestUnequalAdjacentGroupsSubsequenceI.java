a5fc79a65bc01d49f13fbcca32cb786ceafe6378
KhushiBhardwaj28 <163660656+KhushiBhardwaj28@users.noreply.github.com>
2026-01-05 16:04:15 +0530

    File updated at 5/1/2026, 4:04:16 pm

--- 2900-LongestUnequalAdjacentGroupsSubsequenceI/2900-LongestUnequalAdjacentGroupsSubsequenceI.java
+++ 2900-LongestUnequalAdjacentGroupsSubsequenceI/2900-LongestUnequalAdjacentGroupsSubsequenceI.java
@@ -1,14 +1,20 @@
-// Last updated: 5/1/2026, 3:27:13 pm
+// Last updated: 5/1/2026, 4:04:16 pm
 1class Solution {
-2    public List<String> getLongestSubsequence(String[] words, int[] groups) {
-3        List<String> ans = new ArrayList<>();
-4        int lastGroup = -1;
-5        for(int i = 0; i<words.length; i++){
-6            if(groups[i]!=lastGroup){
-7                ans.add(words[i]);
-8                lastGroup = groups[i];
-9            }
-10        }
-11        return ans;
-12    }
-13}
+2    public int longestStrChain(String[] words) {
+3        Arrays.sort(words, (a, b) -> a.length() - b.length());
+4        Map<String, Integer> dp = new HashMap<>();
+5        int maxLen = 1;
+6        for (String word : words) {
+7            int best = 1;
+8            for (int i = 0; i < word.length(); i++) {
+9                String prev = word.substring(0, i) + word.substring(i + 1);
+10                if (dp.containsKey(prev)) {
+11                    best = Math.max(best, dp.get(prev) + 1);
+12                }
+13            }
+14            dp.put(word, best);
+15            maxLen = Math.max(maxLen, best);
+16        }
+17        return maxLen;
+18    }
+19}

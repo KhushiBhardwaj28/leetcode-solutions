bdaf8f6d661a4f183f9ebacbf9a4c7003071312c
KhushiBhardwaj28 <163660656+KhushiBhardwaj28@users.noreply.github.com>
2026-01-27 15:26:47 +0530

    File updated at 27/1/2026, 3:26:45 pm

--- 844-BackspaceStringCompare/844-BackspaceStringCompare.java
+++ 844-BackspaceStringCompare/844-BackspaceStringCompare.java
@@ -1,43 +1,27 @@
-// Last updated: 27/1/2026, 2:12:33 pm
+// Last updated: 27/1/2026, 3:26:45 pm
 1class Solution {
-2    public boolean backspaceCompare(String s, String t) {
-3        Stack<Character> st = new Stack<>();
-4        Stack<Character> sb = new Stack<>();
-5
-6        for(char ch: s.toCharArray()){
-7            if(ch == '#'){
-8                if(!st.isEmpty()){
-9                    st.pop();
-10                }else{
-11                    continue;
-12                }
-13            }else{
-14                st.push(ch);
+2    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+3        Stack<Integer> st = new Stack<>();
+4        HashMap<Integer, Integer> map = new HashMap<>();
+5        for(int i = nums2.length-1; i>=0; i--){
+6            while(!st.isEmpty() && nums2[i]>st.peek()){
+7                st.pop();
+8
+9            }
+10            if(st.isEmpty()){
+11                map.put(nums2[i], -1);
+12            } 
+13            else{
+14                map.put(nums2[i], st.peek());
 15            }
-16            
+16            st.push(nums2[i]);
 17        }
-18        for(char ch: t.toCharArray()){
-19            if(ch == '#'){
-20                if(!sb.isEmpty()){
-21                    sb.pop();
-22                }else{
-23                    continue;
-24                }
-25            }else{
-26                sb.push(ch);
-27            }
-28            
-29        }
-30        if(st.size()!=sb.size()){
-31            return false;
-32        }
-33
-34        while(!st.isEmpty() && !sb.isEmpty()){
-35                if(st.pop()!=sb.pop()){
-36                return false;
-37            }
-38            
-39        }
-40        return true;
-41    }
-42}
+18        for(int i = 0; i<nums1.length; i++){
+19            if(map.containsKey(nums1[i])){
+20                nums1[i] = map.get(nums1[i]);
+21            }
+22        }
+23        return nums1;
+24
+25    }
+26}

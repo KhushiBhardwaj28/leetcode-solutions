f67fda0bb7650d8b23a3fd9bd5b4be08f6a87ad0
KhushiBhardwaj28 <163660656+KhushiBhardwaj28@users.noreply.github.com>
2026-01-27 17:00:48 +0530

    File updated at 27/1/2026, 5:00:46 pm

--- 2073-TimeNeededtoBuyTickets/2073-TimeNeededtoBuyTickets.java
+++ 2073-TimeNeededtoBuyTickets/2073-TimeNeededtoBuyTickets.java
@@ -1,19 +1,15 @@
-// Last updated: 27/1/2026, 4:38:30 pm
+// Last updated: 27/1/2026, 5:00:46 pm
 1class Solution {
 2    public int timeRequiredToBuy(int[] tickets, int k) {
-3        Queue<Integer> q = new LinkedList<>();
-4        for(int i = 0; i<tickets.length; i++){
-5            q.add(i);
-6        }
-7        int ans = 0;
-8        while(!q.isEmpty()){
-9            ans++;
-10            int front = q.poll();
-11            
-12            tickets[front]--;
-13            if(tickets[front] == 0 && front == k) return ans;
-14            if(tickets[front]!=0) q.offer(front);
-15        }
-16        return ans;
-17    }
-18}
+3        int ans = 0;
+4        int x = tickets[k];
+5        for(int i = 0; i<=k; i++){
+6            ans += Math.min(x, tickets[i]);
+7        }
+8        for(int i = k+1; i<tickets.length; i++){
+9            ans+= Math.min(tickets[i],x-1);
+10        }
+11        return ans;
+12
+13    }
+14}

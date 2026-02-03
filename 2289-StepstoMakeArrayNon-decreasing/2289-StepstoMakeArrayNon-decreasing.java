fbe78419829465d0dc87e2d93d75037c01248d5c
KhushiBhardwaj28 <163660656+KhushiBhardwaj28@users.noreply.github.com>
2026-02-03 14:31:57 +0530

    Create 2289-StepstoMakeArrayNon-decreasing

---
+++ 2289-StepstoMakeArrayNon-decreasing/2289-StepstoMakeArrayNon-decreasing.java
@@ -0,0 +1,20 @@
+// Last updated: 3/2/2026, 2:31:55 pm
+1class Solution {
+2    public int totalSteps(int[] nums) {
+3        Stack<int[]> st = new Stack<>();
+4        
+5        int max_c = 0;
+6        for(int i = nums.length-1; i>=0; i-- ){
+7            int c = 0;
+8            while(!st.isEmpty() && nums[st.peek()[0]]<nums[i]){
+9                int[] a = st.pop();
+10                c++;
+11                c = Math.max(c, a[1]);
+12            }
+13            max_c = Math.max(max_c,c);
+14            st.push(new int[]{i, c});
+15        }
+16        return max_c;
+17
+18    }
+19}

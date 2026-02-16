d26c05913bda5e0d707fcc40769b639603aaa4d7
KhushiBhardwaj28 <163660656+KhushiBhardwaj28@users.noreply.github.com>
2026-02-16 14:24:26 +0530

    File updated at 16/2/2026, 2:24:26 pm

--- 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.java
+++ 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.java
@@ -1,4 +1,4 @@
-// Last updated: 16/2/2026, 2:18:52 pm
+// Last updated: 16/2/2026, 2:24:26 pm
 1/**
 2 * Definition for a binary tree node.
 3 * public class TreeNode {
@@ -16,17 +16,22 @@
 15 */
 16class Solution {
 17    public boolean isBalanced(TreeNode root) {
-18        return dfsHeight(root) != -1;
-19    }
-20    private static int dfsHeight(TreeNode root){
-21        if(root == null){
-22            return 0;
-23        }
-24        int lh = dfsHeight(root.left);
-25        if(lh == -1) return -1;
-26        int rh = dfsHeight(root.right);
-27        if(rh == -1) return -1;
-28        if(Math.abs(lh-rh)>1) return -1;
-29        return Math.max(lh,rh)+1;
-30    }
-31}
+18        return helper(root) != -1; 
+19
+20    }
+21    public int helper(TreeNode root){
+22        if(root == null){
+23            return 0;
+24        }
+25        int lh = helper(root.left);
+26        if(lh == -1){
+27            return -1;
+28        }
+29        int rh = helper(root.right);
+30        if(rh == -1){
+31            return -1;
+32        }
+33        if(Math.abs(lh-rh)>1) return -1;
+34        return Math.max(lh, rh)+1;
+35    }
+36}
